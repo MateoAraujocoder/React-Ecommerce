aafffc675c27b5634fbc568d80b6cefdf34025a5
Mateo Araujo <oliaramateo@gmail.com>
2023-10-05 09:37:01 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './componets/Navbar'
+import ItemListContainer from './componets/itemListContainer'
+
+const App = () => {
+  return (
+    <div>
+<Navbar />
+<ItemListContainer greeting= {"Bienvenidos a Samsung"} />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/componets/NavBar.jsx
@@ -0,0 +1,14 @@
+import CartWidget from './CartWidget'
+
+const Navbar = () => {
+  return (
+    <div>
+
+<h1>Samsung Ecommerce</h1>
+  <CartWidget />
+
+    </div>
+  )
+}
+
+export default Navbar

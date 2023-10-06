283a9edba9083dfec25cc23c74ed930b454350a3
Mateo Araujo <oliaramateo@gmail.com>
2023-10-06 12:46:02 -0300

    centro texto y acomodo carrito

--- src/componets/ItemListContainer.jsx
+++ src/componets/ItemListContainer.jsx
@@ -1,7 +1,14 @@
-const ItemListContainer = ({greeting}) => {
-    return (
-      <div>{greeting}</div>
-    )
-  }
-  
-  export default ItemListContainer
+import React from 'react';
+import { Box, Center, Text } from '@chakra-ui/react';
+
+const ItemListContainer = ({ greeting }) => {
+  return (
+    <Center h="100vh"> {/* Centra verticalmente en la página */}
+      <Box>
+        <Text fontSize="2xl">{greeting}</Text>
+      </Box>
+    </Center>
+  );
+}
+
+export default ItemListContainer;
--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Box, Text, Icon } from '@chakra-ui/react';
+import { Box, Text, Icon, Flex } from '@chakra-ui/react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import CartWidget from './CartWidget';
 
 const Navbar = () => {
   return (
-    <Box
-      display="flex"
+    <Flex
       alignItems="center"
       justifyContent="space-between"
       padding="1rem"
@@ -16,18 +15,21 @@ const Navbar = () => {
       <Text fontSize="2xl" fontWeight="bold">
         Samsung Ecommerce
       </Text>
-      <Box
-        as={AiOutlineShoppingCart}
-        fontSize="2xl"
-        ml="2"
-        cursor="pointer"
-        _hover={{
-          color: 'teal.300',
-        }}
-      />
-      <CartWidget />
-    </Box>
+      <Flex alignItems="center">
+        <CartWidget />
+        <Box
+          as={AiOutlineShoppingCart}
+          fontSize="2xl"
+          ml="2"
+          cursor="pointer"
+          _hover={{
+            color: 'teal.300',
+          }}
+        />
+      </Flex>
+    </Flex>
   );
 };
 
 export default Navbar;
+

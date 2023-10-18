e7132b3b6768d8e7c8671f52a8f04e48bcd916bf
Mateo Araujo <oliaramateo@gmail.com>
2023-10-18 12:35:13 -0300

    agrego productos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Box, Text, Button, VStack, Center } from '@chakra-ui/react';
+import { Box, Text, Button, VStack } from '@chakra-ui/react';
 import { Card } from "@chakra-ui/react"
 
 const ItemListContainer = ({ greeting }) => {
+  // Supongamos que tienes un array de productos
+  const products = Array(10).fill().map((_, i) => ({
+    name: `Producto ${i+1}`,
+    price: `$${(i+1)*10}`,
+    stock: `${100-(i*10)} en stock`
+  }));
+
   return (
     <Box>
       <Text fontSize="2xl" mb="4">{greeting}</Text>
-      <Center>
-        <Card maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-          <Box p="6">
-            <VStack>
-              <Button colorScheme="teal" variant="solid">
-                Buy Now
-              </Button>
-            </VStack>
-          </Box>
-        </Card>
-      </Center>
+      <VStack>
+        {products.map((product, i) => (
+          <Card key={i} maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+            <Box p="6">
+              <VStack>
+                <Text fontSize="xl">{product.name}</Text>
+                <Text>{product.price}</Text>
+                <Text>{product.stock}</Text>
+                <Button colorScheme="teal" variant="solid">
+                  Buy Now
+                </Button>
+              </VStack>
+            </Box>
+          </Card>
+        ))}
+      </VStack>
     </Box>
   );
 }
 
 export default ItemListContainer;
+
